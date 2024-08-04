571da8ec782b618610a8c614ae377e64e274f1a5
amasirat <amir.basirat.2004@gmail.com>
2024-08-04 18:57:11 +0330

    added some animations to player movement

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -10,6 +10,8 @@ public partial class Player : CharacterBody2D
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
 
+	AnimatedSprite2D animatedSprite;
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
@@ -20,20 +22,38 @@ public partial class Player : CharacterBody2D
 
 		// Handle Jump.
 		if (Input.IsActionJustPressed("jump") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+			velocity.Y += JumpVelocity;
+
+		if(!IsOnFloor())
+		{
+			animatedSprite.Animation = "jump";
+		}
+		else
+		{
+			animatedSprite.Animation = "idle";
+		}
 
 		// Get the input direction and handle the movement/deceleration.
 		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
+			animatedSprite.Animation = "run";
+			animatedSprite.FlipH = velocity.X < 0;// Flip animatedSprite if direction is to the left(<0)
 		}
 		else
 		{
+			animatedSprite.Animation = "idle";
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
 
 		Velocity = velocity;
 		MoveAndSlide();
+		GD.Print(animatedSprite.Animation);
+	}
+	
+	public override void _Ready()
+	{
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 	}
 }
