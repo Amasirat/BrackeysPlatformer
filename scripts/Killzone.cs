0d553f3e699b7444a143ebb502c27f78429689e8
amasirat <amir.basirat.2004@gmail.com>
2024-08-04 18:36:43 +0330

    made a functional slime

--- scripts/Killzone.cs
+++ scripts/Killzone.cs
@@ -9,12 +9,14 @@ public partial class Killzone : Area2D
     public void OnBodyEntered(Node2D body)
     {
         GD.Print("Died!");
-
+        Engine.TimeScale = 0.5;
+        body.GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
         timer.Start();
     }
 
     public void OnTimeout()
     {
+        Engine.TimeScale = 1;
         GetTree().ReloadCurrentScene();
     }  
 
--- scripts/Slime.cs
+++ scripts/Slime.cs
@@ -1,9 +1,40 @@
 using Godot;
+using System.Collections.Generic;
 
 public partial class Slime : Node2D
 {
+	public override void _Ready()
+	{
+		RayCast = new RayCast2D[2];
+		RayCast[0] = GetNode<RayCast2D>("RayCastRight");
+		RayCast[1] = GetNode<RayCast2D>("RayCastLeft");
+		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
+		if(RayCast[0].IsColliding())
+		{
+			direction = -1;
+			animatedSprite.FlipH = true;
+		}
+
+		if(RayCast[1].IsColliding())
+		{
+			animatedSprite.FlipH = false;
+			direction = 1;
+		}
+		
+		Vector2 pos = Position;
+		pos.X += Speed * (float)delta * direction;
+		Position = new Vector2(pos.X, pos.Y);
 	}
+
+	[Export]
+	public float Speed;
+
+	public int direction = 1;
+	private RayCast2D[] RayCast;
+	private AnimatedSprite2D animatedSprite;
 }
